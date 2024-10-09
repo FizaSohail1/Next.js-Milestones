20ed930a6787ca6b0694c4376810d37ce5c98b30
FizaSohail1 <fizasohail512@gmail.com>
2024-10-09 20:50:29 +0500

    Milestone-1

---
+++ milestone-1/my-app/app/page.tsx
@@ -0,0 +1,15 @@
+
+export default function Home() {
+  return (
+    <>
+    <h1 className="mt-5 p-1 text-white text-center font-bold text-3xl border-white border-solid border-x-4 border-y-4 animate-colorChange">
+      Milestone: 1
+    </h1>
+    <div className="mx-auto mt-48 h-32 p-4 rounded-lg w-2/5 border-white border-solid border-x-4 border-y-4 animate-bounce">
+      <h1 className="text-white text-center my-5 font-bold text-5xl">
+        Hello <span className="text-orange-600">World!</span>
+      </h1>
+    </div>
+  </>
+  );
+}
